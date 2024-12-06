0fbdc3762df9cffa49da092eeb316f9600bd350d
glorianaz <150281309+glorianaz@users.noreply.github.com>
2024-12-06 13:08:57 -0500

    Insertion Sort Implementation

--- app/src/main/java/searchsort/InsertionSort.java
+++ app/src/main/java/searchsort/InsertionSort.java
@@ -5,6 +5,18 @@ public class InsertionSort {
      * Sort the array arr using insertion sort algorithm.
      */
     public static void sort(int[] arr) {
+        for(int i = 1; i < arr.length;i++)
+        {
+            int key = arr[i];
 
+        }
+        int j = i -1;
+
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
     }
 }
