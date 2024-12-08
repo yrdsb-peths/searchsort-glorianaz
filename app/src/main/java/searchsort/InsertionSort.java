633317ab319b355ec9b5028f3631ed9b2a3f698c
glorianaz <150281309+glorianaz@users.noreply.github.com>
2024-12-08 16:52:23 -0500

    Insertion Sort  making variables in scope

--- app/src/main/java/searchsort/InsertionSort.java
+++ app/src/main/java/searchsort/InsertionSort.java
@@ -8,16 +8,14 @@ public class InsertionSort {
         for(int i = 1; i < arr.length;i++)
         {
             int key = arr[i];
+            int j = i -1;
 
+            while (j >= 0 && arr[j] > key)
+            {  
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
         }
-        int j = i -1;
-
-        int key;
-                while (j >= 0 && arr[j] > key)
-        {
-            arr[j + 1] = arr[j];
-            j--;
-        }
-        arr[j + 1] = key;
     }
 }
