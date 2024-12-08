df2ff965a9fc783fdba17d3e64a184666ab00837
glorianaz <150281309+glorianaz@users.noreply.github.com>
2024-12-08 17:56:09 -0500

    Linear search implementation

--- app/src/main/java/searchsort/LinearSearch.java
+++ app/src/main/java/searchsort/LinearSearch.java
@@ -6,6 +6,16 @@ public class LinearSearch {
      * Return -1 if target is not found in arr.
      */
     public static int find(int[] arr, int target) {
-        return 0;
+        // iterate through the array 
+        for(int i = 0l i < arr.length; i++)
+        {
+            if(arr[i] == tagret)
+            {
+                // returning index if target is found
+                return i;
+            }
+        }
+        // returning -1 if target isnt found
+        return -1;
     }
 }
