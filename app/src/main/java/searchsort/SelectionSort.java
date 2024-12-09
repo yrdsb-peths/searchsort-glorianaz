c23340ddbb2bb80b76338ae974bd64b600d9bbb4
glorianaz <150281309+glorianaz@users.noreply.github.com>
2024-12-09 16:55:13 -0500

    selection sort

--- app/src/main/java/searchsort/SelectionSort.java
+++ app/src/main/java/searchsort/SelectionSort.java
@@ -5,7 +5,10 @@ public class SelectionSort {
      * Sort the array arr using selection sort algorithm.
      */
     public static void sort(int[] arr) {
+
+
         for(int i = 0; i < arr.length - 1; i++) {
+            // min index
             int index = i;
 
             for(int j = i; j < arr.length; j++) {
@@ -13,7 +16,13 @@ public class SelectionSort {
                     index = j;
                 }
             }
-            Util.exch(arr, i, index);
+            
+            // swap found element with index i
+            if (index != i) {
+                int temp = arr[i];
+                arr[i] = arr[index];
+                arr[index] = temp;
+            }
         }
     }
 }
