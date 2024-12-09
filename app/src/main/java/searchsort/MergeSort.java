882db4d496593a12684da9f821f3069e1c84a937
glorianaz <150281309+glorianaz@users.noreply.github.com>
2024-12-09 16:17:43 -0500

    Merge Sort Implementation 2
    added comments as well

--- app/src/main/java/searchsort/MergeSort.java
+++ app/src/main/java/searchsort/MergeSort.java
@@ -6,14 +6,61 @@ public class MergeSort extends Sort {
      */
     @Override
     public void sort(int[] arr) {
-        
+        // create an auxilary array for merging
+        int[] aux = new int[arr.length];
+        mergeSort(arr, aux, 0, arr.length - 1);
+    }
+
+    public void mergeSort(int[]arr, int[] aux, int low, int high) {
+        // base case 
+        if(high <= low)
+        {
+            return;
+        }
+        // Find the middle index to split the array
+        int mid = (low + high) / 2;
+
+        // recursively sorting the array
+        mergeSort(arr, aux, low, high);
+        mergeSort(arr, aux, mid + 1, high);
+
+        // merge the 2 sorted halves
+        merge(arr, aux, low, mid, high);
     }
 
     /*
      * Merge the two sorted arrays left and right into the array arr.
      */
-    private void merge(int[] arr, int[] left, int[] right) {
-    
+    private void merge(int[] arr, int[] aux, int left, int mid, int right) {
+        for(int k = left; k <= right; k++)
+        {
+            aux[k] = arr[k];
+        }
+
+        // initialize pointers 
+        int i = left;
+        int j = mid + 1;
+        for(int k = left; k <= right; k++)
+        {
+            if(i > mid){
+                arr[k] = aux[j];
+                j++;
+            }
+            else if(j > right)
+            {
+                arr[k] = aux[j];
+                i++;
+            }
+            else if (aux[j] < aux[i])
+            {
+                arr[k] = aux[j];
+                j++;
+            }
+            else {
+                arr[k] = aux[i];
+                i++;
+            }
+        }
     }
 
 
