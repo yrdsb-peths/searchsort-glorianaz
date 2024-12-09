5210fb7e02a6fbe68bb9e77ff8973db09b4b4562
glorianaz <150281309+glorianaz@users.noreply.github.com>
2024-12-09 13:28:29 -0500

    Quick sort

--- app/src/main/java/searchsort/QuickSort.java
+++ app/src/main/java/searchsort/QuickSort.java
@@ -6,14 +6,37 @@ public class QuickSort extends Sort {
      */
     @Override
     public void sort(int[] arr) {
+        int n = arr.length;
+        for(int i = 0; i < n - 1; i++)
+        {
+            int minIndex = i;
 
+            for(int j = i + 1; j < n; j++)
+            {
+                if(arr[j] < arr[minIndex])
+                {
+                    minIndex = j;
+                }
+            }
+
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    
     }
 
     /*
      * Sort the subarray arr[low..high] using quick sort algorithm.
      */
     private void quickSort(int[] arr, int low, int high) {
-
+        if(high <= low)
+        {
+            return;
+        }
+        int p = partition(arr, low, high);
+        quickSort(arr, low, p - 1);
+        quickSort(arr, p + 1, high);
     }
 
     /*
@@ -21,7 +44,33 @@ public class QuickSort extends Sort {
      * Return the index of the pivot element after partition.
      */
     private int partition(int[] arr, int low, int high) {
-        return 0;
+        int i = low;
+        int j = high + 1;
+        while(true)
+        {
+            while(arr[++1] < arr[low])
+            {
+                if(j == low)
+                {
+                    break;
+                }
+            }
+
+            while(arr[--j] > arr[low])
+            {
+                if(j == low)
+                {
+                    break;
+                }
+            }
+            if(i >= j)
+            {
+                break;
+            }
+            Util.exch(arr, i, j);
+        }
+        Util.exch(arr, low, j);
+        return j;
 
     }
 
--- app/src/main/java/searchsort/SelectionSort.java
+++ app/src/main/java/searchsort/SelectionSort.java
@@ -5,6 +5,16 @@ public class SelectionSort {
      * Sort the array arr using selection sort algorithm.
      */
     public static void sort(int[] arr) {
+        int n = arr.length;
+    }
 
+    for(int i = 0; i < n -1; i++)
+    {
+        // find the min element 
+        int minIndex = i;
+        for(int j = i; j < n; j++)
+        {
+            
+        }
     }
 }
